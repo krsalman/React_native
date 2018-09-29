159b44b7528dc786e50e9a5bb52b5caf50184880
Salman Ahmed <salman.ahmed@alpineinfosolutions.com>
2018-09-29 15:08:18 +0530

    App.js updated

--- sample/App.js
+++ sample/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={ createStore }>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text> HELLO  </Text>
       </View>
+      </Provider>
     );
   }
 }
